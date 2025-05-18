30a5f4fdc6dc3172402b0edc6630e8ac1ae84329
jemonemcc2 <jemonemcc2@gmail.com>
2025-05-18 11:01:37 +0000

    v64

--- test_game.py
+++ test_game.py
@@ -19,14 +19,14 @@ class TestCombat(unittest.TestCase):
         self.assertEqual(new_player_health, initial_player_health)
         self.assertTrue(enemy_defeated)
 
-    def test_no_combat_when_positions_differ(self):
+def test_no_combat_when_positions_differ(self):
         initial_enemy_health = 10
         initial_player_health = 10
         new_enemy_health, enemy_defeated, new_player_health = handle_combat([0, 0], [1, 1], initial_enemy_health, initial_player_health)
         self.assertEqual(new_enemy_health, initial_enemy_health)
- self.assertEqual(new_player_health, initial_player_health)
-        self.assertEqual(new_player_health, initial_player_health)
         self.assertFalse(enemy_defeated)
+        self.assertEqual(new_player_health, initial_player_health) # This is line 27
+
 
     def test_no_combat_when_enemy_defeated(self):
         initial_enemy_health = 0
