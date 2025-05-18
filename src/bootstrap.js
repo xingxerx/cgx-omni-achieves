db3dcd44a61ec3e7b8c5058e1d1f8c3444c68103
jemonemcc2 <jemonemcc2@gmail.com>
2025-05-18 09:31:07 +0000

    v51

---
+++ src/bootstrap.js
@@ -0,0 +1,13 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    firebase: {
+      apiKey: "AIzaSyALmBFR1A6cP8r9qqV2qenwqBxIbOHzABY",
+      authDomain: "cosmoscode-7db61.firebaseapp.com",
+      projectId: "cosmoscode-7db61",
+      storageBucket: "cosmoscode-7db61.firebasestorage.app",
+      messagingSenderId: "715610910634",
+      appId: "1:715610910634:web:cf981d8f39b206a86585d3",
+    },
+    
+    
+    geminiApiKey: "AIzaSyDpM5CejvxEy08W3_wN_yfZ_Qxc7B_zEXw"
+    };
