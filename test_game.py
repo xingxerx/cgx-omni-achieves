b96fc8c508da9864d367e5c9aa19c1d04e4d0a4d
jemonemcc2 <jemonemcc2@gmail.com>
2025-05-20 04:59:59 +0000

    v65

--- test_game.py
+++ test_game.py
@@ -1,17 +1,4 @@
-import unittest
-from chronicle_fates.main import handle_combat
-
-class TestCombat(unittest.TestCase):
-
-    def test_combat_reduces_enemy_health(self):
-        initial_enemy_health = 10
-        initial_player_health = 10
-        new_enemy_health, enemy_defeated, new_player_health = handle_combat([0, 0], [0, 0], initial_enemy_health, initial_player_health)
-        self.assertEqual(new_enemy_health, initial_enemy_health - 1)
-        self.assertFalse(enemy_defeated)
-        self.assertEqual(new_player_health, initial_player_health)
-
-    def test_enemy_defeated_at_zero_health(self):
+def test_enemy_defeated_at_zero_health(self):
         initial_enemy_health = 1
         initial_player_health = 10
         new_enemy_health, enemy_defeated, new_player_health = handle_combat([0, 0], [0, 0], initial_enemy_health, initial_player_health)
@@ -19,7 +6,7 @@ class TestCombat(unittest.TestCase):
         self.assertEqual(new_player_health, initial_player_health)
         self.assertTrue(enemy_defeated)
 
-def test_no_combat_when_positions_differ(self):
+def test_no_combat_when_positions_differ(self): # This is the problematic line (around line 27 in the traceback)
         initial_enemy_health = 10
         initial_player_health = 10
         new_enemy_health, enemy_defeated, new_player_health = handle_combat([0, 0], [1, 1], initial_enemy_health, initial_player_health)
@@ -28,14 +15,10 @@ def test_no_combat_when_positions_differ(self):
         self.assertEqual(new_player_health, initial_player_health) # This is line 27
 
 
-    def test_no_combat_when_enemy_defeated(self):
+    def test_no_combat_when_enemy_defeated(self): # This is line 31 in the traceback
         initial_enemy_health = 0
         initial_player_health = 10
         new_enemy_health, enemy_defeated, new_player_health = handle_combat([0, 0], [0, 0], initial_enemy_health, initial_player_health)
         self.assertEqual(new_enemy_health, 0)
         self.assertEqual(new_player_health, initial_player_health)
         self.assertTrue(enemy_defeated)
-
-
-if __name__ == '__main__':
-    unittest.main()
